d396bcf431c4d274689bfa495026fa9b97472a6b
Chirag <me@chi6rag.net>
2016-09-15 01:18:57 +0530

    Showing timer on the UI

--- wear/src/main/java/com/example/android/sunshine/app/SunshineWatchFaceService.java
+++ wear/src/main/java/com/example/android/sunshine/app/SunshineWatchFaceService.java
@@ -1,6 +1,9 @@
 package com.example.android.sunshine.app;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.view.SurfaceHolder;
 
 public class SunshineWatchFaceService extends CanvasWatchFaceService {
     @Override
@@ -8,7 +11,52 @@ public class SunshineWatchFaceService extends CanvasWatchFaceService {
         return new Engine();
     }
 
-    private class Engine extends CanvasWatchFaceService.Engine {
+    private class Engine extends CanvasWatchFaceService.Engine implements TimerCallback {
+        private SunshineTimer sunshineTimer;
 
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+            sunshineTimer = new SunshineTimer(this);
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+            toggleTimerState(visible);
+            invalidate();
+        }
+
+        private void toggleTimerState(boolean visible) {
+            if (visible)
+                sunshineTimer.start();
+            else
+                sunshineTimer.reset();
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            super.onDraw(canvas, bounds);
+            SunshineWatchFaceCanvas sunshineWatchFaceCanvas = new SunshineWatchFaceCanvas(canvas, bounds, getResources(), isVisible(), isInAmbientMode());
+            sunshineWatchFaceCanvas.draw(sunshineTimer.formattedTime());
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            toggleTimerState(isVisible() && !inAmbientMode);
+            invalidate();
+        }
+
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            invalidate();
+        }
+
+        @Override
+        public void onTimeUpdate() {
+            invalidate();
+        }
     }
 }
